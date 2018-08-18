1a4ee2fdf706d787e3a7e51071bab2e692f1724f
Azarias Boutin <azarias.boutin@gmail.com>
2018-08-18 10:07:24 +0200

    Spaceship hitting

--- scripts/Body.gd
+++ scripts/Body.gd
@@ -5,10 +5,20 @@ extends RigidBody2D
 # var b = "textvar"
 export(int) var rotation_speed = 200
 export(int) var move_speed = 2000
+export(float) var invincible_time = 2.0
+export(float)var max_health = 100
+
 var input_rotation = 0
 var speed_multiplier = 1
-onready var cam = $Camera2D
 var globals
+var health = max_health
+var tween_values = [Color(1,1,1,1), Color(1,0,0,0.5)]
+var flipped = false
+
+onready var cam = $Camera2D
+onready var tween = $Tween
+
+var invicible_timer = null
 
 func _ready():
 	globals = get_tree().root.get_node("/root/globals")
@@ -17,6 +27,7 @@ func _ready():
 	#cam.limit_top = globals.BORDERS.position.y
 	#cam.limit_bottom = globals.BORDERS.end.y
 	set_process(true)
+
 
 func get_input():
 	if Input.is_action_pressed("ui_left"):
@@ -64,7 +75,43 @@ func _physics_process(delta):
 		position.x = cam.limit_right
 	elif c_pos.x > cam.limit_right - 50:
 		position.x = cam.limit_left
+	
+	if invicible_timer != null: return
+	
+	var bodies = $Area2D.get_overlapping_bodies()
+	if bodies.size() > 0:
+		hit_by_spaceship()
 
+
+func hit_by_spaceship():
+		health -= 10
+		# update life display
+		$Area2D/CollisionPolygon2D2.disabled = true
+		invicible_timer = Timer.new()
+		invicible_timer.wait_time = invincible_time
+		invicible_timer.one_shot = true
+		invicible_timer.connect("timeout", self, "end_invincibility")
+		invicible_timer.start()
+		add_child(invicible_timer)
+		_start_tween()
+		tween.connect("tween_completed", self, "_start_tween")
+
+func _start_tween(a= null, b = null):
+	if invicible_timer == null:
+		$Sprite.modulate = tween_values[0]
+		flipped = false
+	else:
+		var c1 = tween_values[1] if flipped else tween_values[0]
+		var c2 = tween_values[0] if flipped else tween_values[1]
+		tween.interpolate_property($Sprite, "modulate", c1, c2 ,0.6, Tween.TRANS_LINEAR, Tween.EASE_IN)
+		tween.start()
+		flipped = not flipped
+
+func end_invincibility():
+	if invicible_timer != null:
+		$Area2D/CollisionPolygon2D2.disabled = false
+		remove_child(invicible_timer)
+	invicible_timer = null
 	
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
